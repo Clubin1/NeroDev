c7720f912b074696e731d1cf79394eaba73b0ad0
Justin Tubay <justin.tubay@gmail.com>
2021-07-03 18:05:05 -0400

    contact page

--- src/components/ContactLanding.js
+++ src/components/ContactLanding.js
@@ -8,12 +8,9 @@ import {faPhone} from '@fortawesome/free-solid-svg-icons'
 const ContactLanding = () => {
     return (
         <div className="contactLanding">
-            <h1>Contact Us Below</h1>
-            <p>From graphic design to web development, we offer a lot of services at NeroDev. Our main thing as an agency is web development, but we also do graphic design.<br></br>
-                We can custom make any logos or images fit to your desires to make your company look more professional.</p>
-            <div><FontAwesomeIcon className="icon"
-                    icon={faPhone}/>
-                704-712-3369</div>
+            <h1 className="contactTitle contactSize">Contact Us Below</h1>
+            <p className="contactInfo">Call us, email us, send us a query, we'll respond <span className="textColor">quickly!</span></p>
+            <p className="contactNumber"> 704-712-3369</p>
         </div>
     )
 }
