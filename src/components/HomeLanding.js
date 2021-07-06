0b4a7eb9132a4eff8d1a681545587440c625fb7d
Justin Tubay <justin.tubay@gmail.com>
2021-07-06 15:56:15 -0400

    landing text change

--- src/components/HomeLanding.js
+++ src/components/HomeLanding.js
@@ -4,8 +4,8 @@ import '../Assets/Styles/Home.css'
 const homeLanding = () => {
 
     return (<div className='landing'>
-        <h3 className="landingSub">North America's Top Ranking</h3>
-        <h1 className="landingTitle">Digital Marketing Agency</h1>
+        <h3 className="landingSub">Digital Marketing Agency</h3>
+        <h1 className="landingTitle">Nero Development</h1>
         <a href="/contact">
         <button>
             <span>CONTACT TEAM</span>
